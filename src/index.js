f2a93c3dd21c43a43db09335d4f92192378de477
Michael5366 <qwentin536@gmail.com>
2024-07-30 04:44:13 +0300

    feat: did the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function reverse (n) {
-  
+	
+	
+	let result = '';
+
+	function getRev() {
+		if (result[0] !== '0') {
+			result = result.split('').reverse().join('');
+		} 
+
+		if (result[0] === '0') {
+			result = result.slice(1, result.length);
+			result = result.split('').join('');
+		}
+	}
+
+	if (n === 0) {
+		result = n;
+	}
+
+	if (n < 0) {
+		result = String(n * (-1));
+		getRev();
+	}
+
+	if (n > 0) {
+		result = String(n);
+		getRev();
+	}
+	return result * 1;
+
 }
